d897ed59d3b946b402670b026743758a455d922b
Zin Soe Tun <zinsoetun123@gmail.com>
2024-07-24 22:01:04 -0700

    Python Projects

---
+++ simple shopping cart program.py
@@ -0,0 +1,11 @@
+# shopping cart
+
+item = input("What item would you like to buy?: ")
+price = float(input("What is the price?: "))
+quantity = int(input("How many would you like?: "))
+
+
+total = price * quantity
+
+print(f"You have bought {quantity} x {item} /s")
+print(f"Your total is : ${round(total)}")
